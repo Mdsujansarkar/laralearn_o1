d8a55e75a838b8eab013041384a8d33fcba28e66
Mdsujansarkar <sujansarkar786@gmail.com>
2021-03-12 07:48:59 +0600

    first commit

---
+++ resources/views/frontend/contact.blade.php
@@ -0,0 +1,6 @@
+<h1>Contact us</h1>
+<ul>
+	<li><a href="http://127.0.0.1:8000/about">About</a></li>
+	<li><a href="http://127.0.0.1:8000/contact">Contact</a></li>
+	<li><a href="http://127.0.0.1:8000/">Home</a></li>
+</ul>
---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,7 @@
+<h1>Home page</h1>
+<ul>
+	<li><a href="http://127.0.0.1:8000/about">About</a></li>
+	<li><a href="http://127.0.0.1:8000/contact">Contact</a></li>
+	<li><a href="http://127.0.0.1:8000/">Home</a></li>
+
+</ul>
